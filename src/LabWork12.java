32a577b6c93eff6330d9bfdb53ccaf1704f2e2b1
AlexChornoshchok <aleksblacke@gmail.com>
2017-11-05 13:55:23 +0200

    Lab Work 12

---
+++ src/LabWork12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LabWork12 {
+    public static void main(String[] arg){
+
+        LabWork_12_1();
+        LabWork_12_2();
+    }
+    static void LabWork_12_1() {
+        System.out.println("Hello ItCloud");
+
+        System.out.println("  ***   *   *     *   *");
+        System.out.println("    *  * *   *   *   * *");
+        System.out.println("    * *   *  *   *  *   *");
+        System.out.println("*   * *****   * *   *****");
+        System.out.println(" ***  *   *    *    *   *");
+    }
+    static void LabWork_12_2() {
+            System.out.println("Wat is you Name?");
+            Scanner ScNew = new Scanner(System.in);
+            String line = ScNew.nextLine();
+            System.out.print("Hallo, "+line+"!");
+        }
+}
